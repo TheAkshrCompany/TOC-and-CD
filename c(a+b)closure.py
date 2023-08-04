8a2d8913aded479dc9e9e06e6c7b4a245937bac7
TheAkshrCompany <50172501+TheAkshrCompany@users.noreply.github.com>
2023-08-04 11:07:23 +0530

    Create c(a+b)closure.py
    Write a program to construct DFA for regular expression (a+b) *.

---
+++ c(a+b)closure.py
@@ -0,0 +1,18 @@
+string=input("Enter the string ")
+arr=list(string)
+store=1 #store variable to keep the check of success of iteration
+if(ord(arr[0])==99): # ascii value of c is 99
+   for i in range(1,len(arr)):
+        if(arr[i]=="a" or arr[i]=="b" or arr[i]==None):
+          pass
+           store+=1
+        else: 
+            break
+   print(store)
+   if(store==len(arr)):
+       print("its a valid expression ")
+   else:
+        print("its an invalid expression")
+
+else:
+    print("its not a valid expression ")
